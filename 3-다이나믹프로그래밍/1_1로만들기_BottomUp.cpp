9a5b421ede97c881dfe05ceee69c60e85f0564e0
padoling <s_hi32@naver.com>
2019-03-12 17:22:57 +0900

    다이나믹 프로그래밍
    3월 14일 숙제

---
+++ 3-다이나믹프로그래밍/1_1로만들기_BottomUp.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int d[1000001];
+
+int makingOne(int n) {
+  d[0] = 0;
+  d[1] = 0;
+
+  // Base case
+  if (n == 1) {
+    return d[1];
+  } else {
+    // i이 n에 도달할 때까지 증가하면서 배열 d에 모든 값을 저장.
+    for (int i=2; i<=n; i++) {
+      if (i%3 == 0) {
+        // i가 3으로 나누어 떨어질 때
+        d[i] = (1 + min(d[i-1], d[i/3]));
+      } else if (i%2 == 0) {
+        // i가 2로 나누어 떨어질 때
+        d[i] = (1 + min(d[i-1], d[i/2]));
+      } else {
+        // 나머지 경우
+        d[i] = (1 + d[i-1]);
+      }
+    }
+  }
+  return d[n];
+}
+
+int main() {
+  int N;
+  cin >> N;
+  cout << makingOne(N) << '\n';
+  return 0;
+}
---
+++ 3-다이나믹프로그래밍/1_1로만들기_TopDown.cpp
@@ -0,0 +1,32 @@
+﻿// 재귀호출 방식
+// 백준 시간초과
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int makingOne(int n) {
+  // Base case
+  if (n == 1) {
+    return 0;
+  } else {
+    if (n%3 == 0) {
+      // n이 3으로 나누어 떨어질 때
+      return (1 + min(makingOne(n/3), makingOne(n-1)));
+    } else if (n%2 == 0) {
+      // n이 2로 나누어 떨어질 때
+      return (1 + min(makingOne(n/2), makingOne(n-1)));
+    } else {
+      // 나머지 case
+      return (1 + makingOne(n-1));
+    }
+  }
+}
+
+int main() {
+  int N;
+  cin >> N;
+  cout << makingOne(N) << '\n';
+  return 0;
+}
